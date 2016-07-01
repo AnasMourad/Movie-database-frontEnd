44146082f084362c22f9b5050e9f41a44c34bcbb
= <=>
2016-07-01 03:09:56 -0500

    Switch views enabled
    now grid/list views are working, and switching between views are
    enabled!

--- js/photos.js
+++ js/photos.js
@@ -8,8 +8,25 @@ $( document ).ready(function() {
         $.get("photo.html", function (template) {
             var html_maker = new htmlMaker(template); //create an html Maker
             var html = html_maker.getHTML(movies["movies"]); //generate dynamic HTML based on the data
-            $("#movies-grid").html(html);
+            if($("#movies-grid").length==0){
+                $("#movies-list").html(html);
+            }else {
+                $("#movies-grid").html(html);
+            }
 
+    });
+    $("#list_icon").on('click', function(){
+
+        $("#movies-grid").attr("id", "movies-list");
+        $("#list_icon").attr("src", "images/list_pressed.jpg");
+        $("#grid_icon").attr("src", "images/grid.jpg");
+
+    });
+    $("#grid_icon").on('click', function(){
+
+        $("#movies-list").attr("id", "movies-grid");
+        $("#list_icon").attr("src", "images/list.jpg");
+        $("#grid_icon").attr("src", "images/grid_pressed.jpg");
 
     });
 });
